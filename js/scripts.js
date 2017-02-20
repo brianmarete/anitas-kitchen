8fcbc460d49acc625d1227e2317b6d32ca864cf6
Brian Marete <bmarete10@gmail.com>
2017-02-20 14:54:03 +0300

    Fix hover effect on meal divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
-$(function () {
-  $('.navbar-toggle, nav').click(function () {
-    $('.navbar-toggle').toggleClass('navbar-on');
-    $('nav').fadeToggle();
-    $('nav').removeClass('nav-hide');
+"use strict";
+
+$(document).ready(
+  $(function () {
+    $('.navbar-toggle, nav').click(function () {
+      $('.navbar-toggle').toggleClass('navbar-on');
+      $('nav').fadeToggle();
+      $('nav').removeClass('nav-hide');
+    });
   });
-});
+);
