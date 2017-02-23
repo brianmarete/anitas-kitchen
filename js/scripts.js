57ae9254f1c1a45bd06629bc8f20f96dcfd8962f
Brian Marete <bmarete10@gmail.com>
2017-02-23 10:19:23 +0300

    Fix menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-$(document).ready(
-  $(function () {
-    $('.navbar-toggle, nav').click(function () {
-      $('.navbar-toggle').toggleClass('navbar-on');
-      $('nav').fadeToggle();
-      $('nav').removeClass('nav-hide');
-    });
+$(document).ready(function () {
+  'use strict';
+  $('.navbar-toggle, nav').click(function () {
+    $('.navbar-toggle').toggleClass('navbar-on');
+    $('nav').fadeToggle();
+    $('nav').removeClass('nav-hide');
   });
-);
+});
